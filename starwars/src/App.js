0adecd34bc6f302f9460dbf5cf2428fdd7fb73ae
Duraan Ali <16447314+duraanali@users.noreply.github.com>
2021-06-24 20:10:18 -0500

    Update App.js

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -5,7 +5,7 @@ const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
-  // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a 
+  // Fetch charactersn and films from the star wars api in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
